5b66f30a21e9831d174b1f4fcc998a71d423ebb3
cromiejunk-dev <cromie.junk@gmail.com>
2026-01-31 18:02:05 -0500

    Set Chromium binary location for Chrome driver

--- check_opinions_chancery.py
+++ check_opinions_chancery.py
@@ -43,6 +43,7 @@ def setup_driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.binary_location = "/snap/bin/chromium"
     
     driver = webdriver.Chrome(options=chrome_options)
     return driver
