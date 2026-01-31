e36d55922a81883625ba6c3c1a535043c9a1f7c2
cromiejunk-dev <cromie.junk@gmail.com>
2026-01-31 18:35:50 -0500

    Update Chrome driver setup for Selenium

--- check_opinions_chancery.py
+++ check_opinions_chancery.py
@@ -37,15 +37,19 @@ def save_seen_opinions(opinions):
 
 def setup_driver():
     """Set up Chrome driver"""
+    from selenium.webdriver.chrome.service import Service
+    
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.binary_location = "/snap/bin/chromium"
     
-    driver = webdriver.Chrome(options=chrome_options)
+    # Use the snap chromium
+    service = Service("/snap/bin/chromium.chromedriver")
+    
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
 def scrape_chancery_opinions():
